2eb4a8028a94180567c7d944d6b56a41c8bcc599
EllieGe-craftswoman <elliegeak@gmail.com>
2022-02-28 12:21:12 +0100

    Exercise 1 tested NegativeNumberValidator and fixed the bug

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,33 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class NegativeNumberValidatorTest {
+
+    private NegativeNumberValidator SUT;
+    @Before
+    public void setUp(){
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void testIsNegativeWithNegative(){
+        boolean result = SUT.isNegative(-4);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void testIsNegativeWithPositive(){
+        boolean result = SUT.isNegative(4);
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void testIsNegativeWithZero(){
+        boolean result = SUT.isNegative(0);
+        Assert.assertFalse(result);
+    }
+    
 
 }
