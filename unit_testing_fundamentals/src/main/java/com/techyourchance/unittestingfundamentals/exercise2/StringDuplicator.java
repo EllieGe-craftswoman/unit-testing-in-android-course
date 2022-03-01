9c2d463d80a6ae5f8d4ee1b7ded8cbcd7a71521c
EllieGe-craftswoman <elliegeak@gmail.com>
2022-03-01 10:41:36 +0100

    Exercise 2 solved and bug fixed

--- unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicator.java
+++ unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicator.java
@@ -6,8 +6,8 @@ public class StringDuplicator {
      * @return concatenation of the argument with itself e.g. "hi" -> "hihi"
      */
     public String duplicate(String string) {
-        // the bug is triplication instead of duplication
-        return string + string + string;
+        // the FIXED bug is triplication instead of duplication
+        return string + string;
     }
 
 }
--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringDuplicatorTest {
 
+    private StringDuplicator SUT;
+
+    @Before
+    public void setUp() {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String result = SUT.duplicate("");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void duplicate_oneCharacter_sameTextReturned() {
+        String result = SUT.duplicate("E");
+        assertEquals("EE", result);
+    }
+
+    @Test
+    public void duplicate_longString_longStringDuplicatedReturned() {
+        String result = SUT.duplicate("Ellie Ge. Soudi is a famous world known software craftswoman icon");
+        assertEquals("Ellie Ge. Soudi is a famous world known software craftswoman iconEllie Ge. Soudi is a famous world known software craftswoman icon", result);
+    }
 }
