bc5f4c4728bf32150acc18f17692cb1599c17881
EllieGe-craftswoman <elliegeak@gmail.com>
2022-03-04 18:23:02 +0100

    Example 5

--- test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/example5/UserInputValidatorTest.java
+++ test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/example5/UserInputValidatorTest.java
@@ -1,43 +1,45 @@
 package com.techyourchance.testdoublesfundamentals.example5;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UserInputValidatorTest {
 
+    public static final String FULL_NAME = "Ellie Soudi";
+    public static final String USERNAME = "Ellie_S";
     UserInputValidator SUT;
 
     @Before
-    public void setup() throws Exception {
+    public void setUp() {
         SUT = new UserInputValidator();
     }
 
     @Test
-    public void isValidFullName_validFullName_trueReturned() throws Exception {
-        boolean result = SUT.isValidFullName("validFullName");
-        assertThat(result, is(true));
+    public void isValidFullName_validFullName_trueReturned() {
+        boolean result = SUT.isValidFullName(FULL_NAME);
+        assertTrue(result);
     }
 
     @Test
-    public void isValidFullName_invalidFullName_falseReturned() throws Exception {
+    public void isValidFullName_invalidFullName_falseReturned() {
         boolean result = SUT.isValidFullName("");
-        assertThat(result, is(false));
+        assertFalse(result);
+    }
+    /* Not Testable due to use of static function which is hidden and can't be seen here
+       And we should avoid mocking statics
+    @Test
+    public void isValidUsername_validUsername_trueReturned() {
+        boolean result = SUT.isValidUsername(USERNAME);
+        assertTrue(result);
     }
 
     @Test
-    public void isValidUsername_validUsername_trueReturned() throws Exception {
-        boolean result = SUT.isValidUsername("validUsername");
-        assertThat(result, is(true));
-    }
-
-    @Test
-    public void isValidUsername_invalidUsername_falseReturned() throws Exception {
+    public void isValidUsername_invalidUsername_falseReturned() {
         boolean result = SUT.isValidUsername("");
-        assertThat(result, is(false));
+        assertFalse(result);
     }
+    */
 }
