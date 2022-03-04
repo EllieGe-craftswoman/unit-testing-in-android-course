152920460d1855ce2f3d4ab27bde3ac20071ef33
EllieGe-craftswoman <elliegeak@gmail.com>
2022-03-04 18:35:08 +0100

    Example 6

--- test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/example6/FitnessTrackerTest.java
+++ test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/example6/FitnessTrackerTest.java
@@ -1,31 +1,41 @@
 package com.techyourchance.testdoublesfundamentals.example6;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 
 public class FitnessTrackerTest {
 
     FitnessTracker SUT;
 
     @Before
-    public void setup() throws Exception {
+    public void setUp(){
         SUT = new FitnessTracker();
     }
 
+    /*Usage of Singletons will cause cross talk between tests
+    * and they affect each  other
+    * Mr Singleton is the worst enemy of testable code because interdependency
+    * occurs with it
+    *
+    * A workaround was done in the below tests using:
+    * int currentSteps = SUT.getTotalSteps();
+    * to be able to count the difference between the new and old count
+     */
     @Test
-    public void step_totalIncremented() throws Exception {
+    public void step_totalIncremented() {
+        int currentSteps = SUT.getTotalSteps();
         SUT.step();
-        assertThat(SUT.getTotalSteps(), is(1));
+        assertEquals(currentSteps + 1, SUT.getTotalSteps());
     }
 
     @Test
-    public void runStep_totalIncrementedByCorrectRatio() throws Exception {
+    public void runStep_totalIncrementedByCorrectRation() {
+        int currentSteps = SUT.getTotalSteps();
         SUT.runStep();
-        assertThat(SUT.getTotalSteps(), is(2));
+        assertEquals(currentSteps + 2, SUT.getTotalSteps());
     }
+
 }
